fda36585fb5f81c8f6cc344b399078b1d24fa441
Michael Job Figueroa <micjobfigueroa@gmail.com>
2020-08-27 14:32:51 +0800

    filter task function

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
 	taskList.addEventListener('click', removeTask);
 	// Clear task event
 	clearBtn.addEventListener('click', clearTasks);
+	// Filter tasks event
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -80,3 +82,17 @@ function clearTasks() {
 // 	taskList.innerHTML = '';
 // }
 // https://jsperf.com/innerhtml-vs-removechild
+
+// Filter Tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+
+	document.querySelectorAll('.collection-item').forEach(function (task) {
+		const item = task.firstChild.textContent;
+		if (item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
+}
